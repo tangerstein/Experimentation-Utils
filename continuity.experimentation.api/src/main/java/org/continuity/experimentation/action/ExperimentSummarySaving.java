573f45b9e5decf8e61dbc635cdf149d7b6f108f7
Henning Schulz <henning.schulz@novatec-gmbh.de>
2017-12-18 18:58:45 +0100

    Added action for saving a summary and added ConcurrentElement.

---
+++ continuity.experimentation.api/src/main/java/org/continuity/experimentation/action/ExperimentSummarySaving.java
@@ -0,0 +1,48 @@
+package org.continuity.experimentation.action;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+import org.continuity.experimentation.Experiment;
+import org.continuity.experimentation.IExperimentAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Henning Schulz
+ *
+ */
+public class ExperimentSummarySaving implements IExperimentAction {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ExperimentSummarySaving.class);
+
+	private final Path dir;
+
+	private final Experiment experiment;
+
+	public ExperimentSummarySaving(Path dir, Experiment experiment) {
+		this.dir = dir;
+		this.experiment = experiment;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void execute() {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd-HH30");
+		String filename = dateFormat.format(new Date()) + "-summary.txt";
+		try {
+			Files.write(dir.resolve(filename), Arrays.asList(experiment.toString().split("\\n")));
+			LOGGER.info("Wrote the experiment summary to {}", dir.resolve(filename));
+		} catch (IOException e) {
+			LOGGER.error("Could not write the experiment summary to {}", dir.resolve(filename));
+			e.printStackTrace();
+		}
+	}
+
+}
