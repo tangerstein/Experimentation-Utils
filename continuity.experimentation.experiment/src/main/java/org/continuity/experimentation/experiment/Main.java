0d17dc33a1cbd2064d356b414f5beaf4b1b6a295
Henning Schulz <henning.schulz@novatec-gmbh.de>
2017-12-29 17:55:15 +0100

    Added experiment project where actual experiment can be executed.

---
+++ continuity.experimentation.experiment/src/main/java/org/continuity/experimentation/experiment/Main.java
@@ -0,0 +1,13 @@
+package org.continuity.experimentation.experiment;
+
+/**
+ * @author Henning Schulz
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO: Implement experiment
+	}
+
+}
