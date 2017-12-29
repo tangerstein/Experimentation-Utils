cd46fc35d959cf85296be11837e710076ab56b13
Henning Schulz <henning.schulz@novatec-gmbh.de>
2017-12-29 17:56:25 +0100

    Initial commit for the ICPE 18 LTB experiments.

--- continuity.experimentation.experiment/src/main/java/org/continuity/experimentation/experiment/Main.java
+++ continuity.experimentation.experiment/src/main/java/org/continuity/experimentation/experiment/Main.java
@@ -7,7 +7,7 @@ package org.continuity.experimentation.experiment;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO: Implement experiment
+		// TODO: Implement the experiment for the vision paper
 	}
 
 }
