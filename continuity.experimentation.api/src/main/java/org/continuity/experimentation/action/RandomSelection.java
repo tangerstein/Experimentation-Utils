4dcfb3ec3d518ad134a8a65857852d043a3db2eb
Henning Schulz <henning.schulz@novatec-gmbh.de>
2018-01-02 10:52:13 +0100

    RandomSelection can be configured to not select the same element twice in a row.

--- continuity.experimentation.api/src/main/java/org/continuity/experimentation/action/RandomSelection.java
+++ continuity.experimentation.api/src/main/java/org/continuity/experimentation/action/RandomSelection.java
@@ -1,5 +1,6 @@
 package org.continuity.experimentation.action;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -9,7 +10,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Selects a random element from an input list and stores it to an output data holder.
+ * Selects a random element from an input list and stores it to an output data holder. Takes care of
+ * not choosing the same element twice in a row (if there are at least two elements).
  *
  * @author Henning Schulz
  *
@@ -21,15 +23,22 @@ public class RandomSelection<T> implements IExperimentAction {
 	private final IDataHolder<List<T>> inputHolder;
 	private final IDataHolder<T> outputHolder;
 
+	private final boolean avoidTwiceInARow;
+
+	private T last = null;
+
 	private final Random random = new Random();
 
 	/**
 	 * @param inputHolder
 	 * @param outputHolder
+	 * @param avoidTwiceInARow
+	 *            If {@code true}, the action won't take the same element twice in a row.
 	 */
-	public RandomSelection(IDataHolder<List<T>> inputHolder, IDataHolder<T> outputHolder) {
+	public RandomSelection(IDataHolder<List<T>> inputHolder, IDataHolder<T> outputHolder, boolean avoidTwiceInARow) {
 		this.inputHolder = inputHolder;
 		this.outputHolder = outputHolder;
+		this.avoidTwiceInARow = avoidTwiceInARow;
 	}
 
 	/**
@@ -37,9 +46,22 @@ public class RandomSelection<T> implements IExperimentAction {
 	 */
 	@Override
 	public void execute() {
-		int size = inputHolder.get().size();
-		T selected = inputHolder.get().get(random.nextInt(size));
+		List<T> selection;
+
+		if (!avoidTwiceInARow) {
+			selection = inputHolder.get();
+		} else {
+			selection = new ArrayList<>(inputHolder.get());
+
+			if ((last != null) && (inputHolder.get().size() > 1)) {
+				selection.remove(last);
+			}
+		}
+
+		int size = selection.size();
+		T selected = selection.get(random.nextInt(size));
 		outputHolder.set(selected);
+		last = selected;
 
 		LOGGER.info("Selected {} from {} elements.", selected, size);
 	}
